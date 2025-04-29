e989cbdc75fc07933fe9bba8ef05e3415c3d7380
homeira kalim <homeirakalim@gmail.com>
2025-04-29 18:42:43 -0400

    final- added contract and customer routes for python flask contracts lab - complete

--- server/app.py
+++ server/app.py
@@ -2,9 +2,30 @@
 
 from flask import Flask, request, current_app, g, make_response
 
-contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
+contracts = [
+    {"id": 1, "contract_information": "This contract is for John and building a shed"},
+    {"id": 2, "contract_information": "This contract is for a deck for a buisiness"},
+    {"id": 3, "contract_information": "This contract is to confirm ownership of this car"}
+]
 customers = ["bob","bill","john","sarah"]
+
 app = Flask(__name__)
+
+@app.route('/contract/<int:id>', methods=['GET'])
+def get_contract(id):
+    for contract in contracts:
+        if contract["id"] == id:
+            response_text = f'Contract Info: {contract["contract_information"]}'
+            return make_response(response_text, 200)
+            # alternatively, from flask import jsonify and use:
+            # return jsonify(contract), 200
+    return make_response({"error":"Contract not found"}, 404)
+
+@app.route('/customer/<string:customer_name>', methods=['GET'])
+def get_customer(customer_name):
+    if customer_name.lower() in customers:
+        return make_response('', 204)
+    return make_response({"error":"Customer not found"}, 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
